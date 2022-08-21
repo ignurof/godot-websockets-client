fcba71e1b599de23b0af338e198106b3687afd42
Ignurof <ignurof@ignurof.com>
2022-08-22 00:50:35 +0200

    network_player

--- maps/demo_map.gd
+++ maps/demo_map.gd
@@ -2,6 +2,10 @@ extends Node2D
 # Map should probably not contain network spawn code, but this is a prototype
 
 var local_player = load("res://players/player.tscn")
+var network_player = load("res://players/network_player.tscn")
+
+onready var player_list = $PlayerList
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,12 +17,20 @@ func _ready():
 func _on_spawn_local_player(player: Dictionary) -> void:
     var lp = local_player.instance()
     lp.position = Vector2(player.x, player.y)
-    add_child(lp)
+    player_list.add_child(lp)
     
     
-func _on_spawn_new_player() -> void:
-    pass
+func _on_spawn_new_player(player: Dictionary) -> void:
+    _spawn_network_player(player)
     
     
-func _on_spawn_network_players() -> void:
-    pass
+func _on_spawn_network_players(players: Array) -> void:
+    for player in players:
+        if player.uuid != Client.uuid:
+            _spawn_network_player(player)
+
+
+func _spawn_network_player(player: Dictionary) -> void:
+    var np = network_player.instance()
+    np.position = Vector2(player.x, player.y)
+    player_list.add_child(np)
